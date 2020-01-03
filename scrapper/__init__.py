21ab4e373d4955b9c311abac1730fa9cb013c783
JulienRouar <julien.rouar@gmail.com>
2020-01-03 21:50:27 +0100

    Initial commit architecture

---
+++ scrapper/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from .scrapper import Scrapper
+
---
+++ scrapper/scrapper.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from selenium import
+
+class Scrapper(self):
+    
+    __slots__ = ('__main_url', )
+    
+    def __init__(self):
+        self.__main_url = 'https://magic.wizards.com/en/content/deck-lists-magic-online-products-game-info'
+        
+    def gatherLinks(self):
+        
+    def gatherDeckLists(self):
+        
+    def readDeckList(self):
+        
+    def run(self):
